df4977fee4579435afb662041c5a97c83141c383
FatSijarina <fs51701@ubt-uni.net>
2022-03-25 10:33:22 +0100

    Add project files.

---
+++ ReStore/API/Data/StoreContext.cs
@@ -0,0 +1,14 @@
+﻿using API.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Data
+{
+    public class StoreContext : DbContext
+    {
+        public StoreContext(DbContextOptions options) : base(options)
+        {
+        }
+        //created a dbset that accepts products
+        public DbSet<Product> Products { get; set; }
+    }
+}
